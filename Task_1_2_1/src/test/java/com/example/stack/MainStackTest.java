73d719c9d3f92b856acf34266ab83876ef0d2d08
nocarend <58514394+nocarend@users.noreply.github.com>
2022-09-10 10:57:36 +0300

    fun:

--- Task_1_2_1/src/test/java/com/example/stack/MainStackTest.java
+++ Task_1_2_1/src/test/java/com/example/stack/MainStackTest.java
@@ -3,7 +3,6 @@ package com.example.stack;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MainStackTest {
