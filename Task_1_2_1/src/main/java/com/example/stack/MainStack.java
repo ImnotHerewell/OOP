77045fe0a87186f550539a8219c348748d3b0668
nocarend <58514394+nocarend@users.noreply.github.com>
2022-09-12 15:38:47 +0300

    tabs?

--- Task_1_2_1/src/main/java/com/example/stack/MainStack.java
+++ Task_1_2_1/src/main/java/com/example/stack/MainStack.java
@@ -6,62 +6,62 @@ package com.example.stack;
  * @param <T> - any Object.
  */
 public class MainStack<T> {
-  int curSize = 0;
-  static final int MAX_SIZE = 10000;
-  public T[] array = (T[]) new Object[MAX_SIZE];
+    int curSize = 0;
+    static final int MAX_SIZE = 10000;
+    public T[] array = (T[]) new Object[MAX_SIZE];
 
-  /**
-   * @param inputElem - Object for push.
-   * @return - checking that we push or not.
-   */
-  boolean push(T inputElem) {
-    if (curSize == MAX_SIZE) return false;
-    this.array[curSize++] = inputElem;
-    return true;
-  }
-
-  /**
-   * @return - checking that stack empty or not.
-   */
-  public T pop() {
-    if (curSize == 0) return null;
-    return this.array[--curSize];
-  }
-
-  /**
-   * @param inputArray - array that should be pushed.
-   * @return - it is possible or not.
-   */
-  public boolean pushStack(T[] inputArray) {
-    if (inputArray.length + curSize >= MAX_SIZE) return false;
-    for (T Elem : inputArray) {
-      this.push(Elem);
+    /**
+     * @param inputElem - Object for push.
+     * @return - checking that we push or not.
+     */
+    boolean push(T inputElem) {
+        if (curSize == MAX_SIZE) return false;
+        this.array[curSize++] = inputElem;
+        return true;
     }
-    return true;
-  }
 
-  /**
-   * @param quantityOfElems - how many elements we want to extract from stack.
-   * @return - null - impossible, notnull - possible.
-   */
-  public MainStack<T> popStack(int quantityOfElems) {
-    MainStack<T> newStack = new MainStack<T>();
-    if (quantityOfElems > curSize) return null;
-    else {
-      for (int indexArray = curSize - quantityOfElems; indexArray < curSize; indexArray++) {
-        newStack.push(this.array[indexArray]);
-      }
-      for (int quantityI = 0; quantityI < quantityOfElems; quantityI++) {
-        this.pop();
-      }
-      return newStack;
+    /**
+     * @return - checking that stack empty or not.
+     */
+    public T pop() {
+        if (curSize == 0) return null;
+        return this.array[--curSize];
     }
-  }
 
-  /**
-   * @return - how many elements in stack.
-   */
-  public int count() {
-    return curSize;
-  }
+    /**
+     * @param inputArray - array that should be pushed.
+     * @return - it is possible or not.
+     */
+    public boolean pushStack(T[] inputArray) {
+        if (inputArray.length + curSize >= MAX_SIZE) return false;
+        for (T Elem : inputArray) {
+            this.push(Elem);
+        }
+        return true;
+    }
+
+    /**
+     * @param quantityOfElems - how many elements we want to extract from stack.
+     * @return - null - impossible, notnull - possible.
+     */
+    public MainStack<T> popStack(int quantityOfElems) {
+        MainStack<T> newStack = new MainStack<T>();
+        if (quantityOfElems > curSize) return null;
+        else {
+            for (int indexArray = curSize - quantityOfElems; indexArray < curSize; indexArray++) {
+                newStack.push(this.array[indexArray]);
+            }
+            for (int quantityI = 0; quantityI < quantityOfElems; quantityI++) {
+                this.pop();
+            }
+            return newStack;
+        }
+    }
+
+    /**
+     * @return - how many elements in stack.
+     */
+    public int count() {
+        return curSize;
+    }
 }
--- Task_1_2_1/src/test/java/com/example/stack/MainStackTest.java
+++ Task_1_2_1/src/test/java/com/example/stack/MainStackTest.java
@@ -5,68 +5,80 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-/** Class with tests. */
+/**
+ * Class with tests.
+ */
 class MainStackTest {
 
-  /** Simple test on push. */
-  @Test
-  void pushTest() {
-    MainStack<String> testStack = new MainStack<>();
-    String test = "baobab";
-    testStack.push(test);
-    String[] expectedResult = new String[MainStack.MAX_SIZE];
-    expectedResult[0] = "baobab";
-    assertArrayEquals(expectedResult, testStack.array);
-  }
+    /**
+     * Simple test on push.
+     */
+    @Test
+    void pushTest() {
+        MainStack<String> testStack = new MainStack<>();
+        String test = "baobab";
+        testStack.push(test);
+        String[] expectedResult = new String[MainStack.MAX_SIZE];
+        expectedResult[0] = "baobab";
+        assertArrayEquals(expectedResult, testStack.array);
+    }
 
-  /** Simple test on pop. */
-  @Test
-  void popTest() {
-    MainStack<String> testStack = new MainStack<>();
-    testStack.push("baobab");
-    String expectedResult = "baobab";
-    String test = testStack.pop();
-    assertEquals(expectedResult, test);
-  }
+    /**
+     * Simple test on pop.
+     */
+    @Test
+    void popTest() {
+        MainStack<String> testStack = new MainStack<>();
+        testStack.push("baobab");
+        String expectedResult = "baobab";
+        String test = testStack.pop();
+        assertEquals(expectedResult, test);
+    }
 
-  /** Simple test on pushStack. */
-  @Test
-  void pushStackTest() {
-    MainStack<String> testStack = new MainStack<>();
-    String[] inputValues = new String[] {"a", "b", "c", "d", "e"};
-    testStack.pushStack(inputValues);
-    String[] expectedResult = new String[MainStack.MAX_SIZE];
-    expectedResult[0] = "a";
-    expectedResult[1] = "b";
-    expectedResult[2] = "c";
-    expectedResult[3] = "d";
-    expectedResult[4] = "e";
-    assertArrayEquals(expectedResult, testStack.array);
-  }
+    /**
+     * Simple test on pushStack.
+     */
+    @Test
+    void pushStackTest() {
+        MainStack<String> testStack = new MainStack<>();
+        String[] inputValues = new String[]{"a", "b", "c", "d", "e"};
+        testStack.pushStack(inputValues);
+        String[] expectedResult = new String[MainStack.MAX_SIZE];
+        expectedResult[0] = "a";
+        expectedResult[1] = "b";
+        expectedResult[2] = "c";
+        expectedResult[3] = "d";
+        expectedResult[4] = "e";
+        assertArrayEquals(expectedResult, testStack.array);
+    }
 
-  /** Simple test on popStack. */
-  @Test
-  void popStackTest() {
-    MainStack<String> testStack = new MainStack<>();
-    String[] inputValues = new String[] {"a", "b", "c", "d", "e"};
-    testStack.pushStack(inputValues);
-    MainStack<String> testResult = testStack.popStack(4);
-    String[] expectedResult = new String[MainStack.MAX_SIZE];
-    expectedResult[0] = "b";
-    expectedResult[1] = "c";
-    expectedResult[2] = "d";
-    expectedResult[3] = "e";
-    assertArrayEquals(expectedResult, testResult.array);
-  }
+    /**
+     * Simple test on popStack.
+     */
+    @Test
+    void popStackTest() {
+        MainStack<String> testStack = new MainStack<>();
+        String[] inputValues = new String[]{"a", "b", "c", "d", "e"};
+        testStack.pushStack(inputValues);
+        MainStack<String> testResult = testStack.popStack(4);
+        String[] expectedResult = new String[MainStack.MAX_SIZE];
+        expectedResult[0] = "b";
+        expectedResult[1] = "c";
+        expectedResult[2] = "d";
+        expectedResult[3] = "e";
+        assertArrayEquals(expectedResult, testResult.array);
+    }
 
-  /** Simple test on count. */
-  @Test
-  void countTest() {
-    MainStack<String> testStack = new MainStack<>();
-    String[] inputValues = new String[] {"a", "b", "c", "d", "e"};
-    testStack.pushStack(inputValues);
-    int expectedResult = 5;
-    int testResult = testStack.count();
-    assertEquals(expectedResult, testResult);
-  }
+    /**
+     * Simple test on count.
+     */
+    @Test
+    void countTest() {
+        MainStack<String> testStack = new MainStack<>();
+        String[] inputValues = new String[]{"a", "b", "c", "d", "e"};
+        testStack.pushStack(inputValues);
+        int expectedResult = 5;
+        int testResult = testStack.count();
+        assertEquals(expectedResult, testResult);
+    }
 }
