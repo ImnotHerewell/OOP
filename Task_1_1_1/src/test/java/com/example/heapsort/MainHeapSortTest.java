04ac22d0b5a52bc99c2a0dc49317032d9f3f25f8
nocarend <58514394+nocarend@users.noreply.github.com>
2022-09-20 16:08:47 +0300

    tyazhko

--- Task_1_1_1/src/test/java/com/example/heapsort/MainHeapSortTest.java
+++ Task_1_1_1/src/test/java/com/example/heapsort/MainHeapSortTest.java
@@ -3,9 +3,8 @@ package com.example.heapsort;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.Arrays;
-
+import java.util.Random;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
