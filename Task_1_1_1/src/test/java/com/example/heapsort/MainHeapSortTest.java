7c2061f4cd6ea5b2409e91923275ba8b0504c0ec
nocarend <58514394+nocarend@users.noreply.github.com>
2022-09-20 16:12:53 +0300

    ok

--- Task_1_1_1/src/test/java/com/example/heapsort/MainHeapSortTest.java
+++ Task_1_1_1/src/test/java/com/example/heapsort/MainHeapSortTest.java
@@ -1,7 +1,7 @@
 package com.example.heapsort;
 
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
