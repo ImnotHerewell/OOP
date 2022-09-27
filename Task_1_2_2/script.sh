c1b33f4df11c98464e2ce367ba77c5af35ebde54
nocarend <58514394+nocarend@users.noreply.github.com>
2022-09-27 12:14:15 +0300

    test

--- Task_1_2_2/script.sh
+++
@@ -1,7 +0,0 @@
-cd src/main/java
-javac ru/nsu/valikov/Tree.java
-java ru.nsu.valikov.Tree
-javadoc -d ./../../../mainjavadoc ru.nsu.valikov
-rm ru/nsu/valikov/Tree.class
-#cd ./../../test/java
-#javadoc -d ./../../../testjavadoc ru.nsu.valikov
--- Task_1_2_2/src/main/java/ru/nsu/valikov/Tree.java
+++ Task_1_2_2/src/main/java/ru/nsu/valikov/Tree.java
@@ -1,5 +1,19 @@
 package ru.nsu.valikov;
 
-public class Tree<T> implements Iterable<T> {
+import java.util.Iterator;
 
+public class Tree<T> implements Iterable<T> {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
 }
