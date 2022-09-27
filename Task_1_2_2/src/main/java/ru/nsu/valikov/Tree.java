0448b302cdba8fa1042a1d028ee5545813fe8560
nocarend <58514394+nocarend@users.noreply.github.com>
2022-09-27 14:36:43 +0300

    test

--- Task_1_2_2/src/main/java/ru/nsu/valikov/Tree.java
+++ Task_1_2_2/src/main/java/ru/nsu/valikov/Tree.java
@@ -1,11 +1,36 @@
 package ru.nsu.valikov;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class Tree<T> implements Iterable<T> {
     public static void main(String[] args) {
-
+        System.out.println("Hello, Tree!");
     }
+
+    private T nodeValue;
+    List<Tree<T>> children= new ArrayList<>();
+
+    public T get(){
+        return nodeValue;
+    }
+
+    public Tree<T> add(T nodeInput){
+        Tree<T> newNode = new Tree<T>();
+        newNode.nodeValue=nodeInput;
+        this.children.add(newNode);
+        return this;
+    }
+
+    public Tree<T> add (Tree<T> curNode, T childValue){
+        Tree<T> newNode = new Tree<T>();
+        curNode.nodeValue=childValue;
+        curNode.children.add(newNode);
+        return newNode;
+    }
+
+    public void remove ()
 
     /**
      * Returns an iterator over elements of type {@code T}.
---
+++ Task_1_2_2/src/test/java/ru/nsu/valikov/TreeTest.java
@@ -0,0 +1,29 @@
+package ru.nsu.valikov;
+
+import org.junit.jupiter.api.Test;
+
+public class TreeTest {
+    @Test
+    void main() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void add() {
+        Tree <String> kek=new Tree<>();
+        Tree <String> tet=new Tree<>();
+        tet=kek.add("a)");
+        kek.add(tet,"B");
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void iterator() {
+    }
+}
