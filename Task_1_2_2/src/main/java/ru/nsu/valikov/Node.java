80ea5c45f266d2822fcfe6c151331173e64e1e96
nocarend <58514394+nocarend@users.noreply.github.com>
2022-09-29 14:50:52 +0300

    version with tests

--- Task_1_2_2/src/main/java/ru/nsu/valikov/Node.java
+++ Task_1_2_2/src/main/java/ru/nsu/valikov/Node.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * @param <T> non-primitive type.
+ */
 public class Node<T> {
     private T value;
     private Node<T> parent;
@@ -11,10 +14,10 @@ public class Node<T> {
     private final List<Node<T>> children;
 
     public Node() {
-        this.childCount = 0;
-        this.children = new ArrayList<>();
-        this.parent = null;
-        this.value = null;
+        childCount = 0;
+        children = new ArrayList<>();
+        parent = null;
+        value = null;
     }
 
     public void setValue(T value) {
@@ -22,7 +25,7 @@ public class Node<T> {
     }
 
     public T getValue() {
-        return this.value;
+        return value;
     }
 
     public void setParent(Node<T> parent) {
@@ -30,39 +33,39 @@ public class Node<T> {
     }
 
     public Node<T> getParent() {
-        return this.parent;
+        return parent;
     }
 
     public Integer getChildCount() {
-        return this.childCount;
+        return childCount;
     }
 
     public void addChild(Node<T> child) {
         if (child != null) {
-            this.childCount++;
-            this.children.add(child);
+            childCount++;
+            children.add(child);
         }
     }
 
     public void delete() {
-        if (this.value != null) {
-            for (Node<T> child : this.children) {
-                child.parent = this.parent;
-                this.parent.addChild(child);
+        if (value != null) {
+            for (Node<T> child : children) {
+                child.parent = parent;
+                parent.addChild(child);
             }
-            this.parent.childCount--;
-            this.parent.children.remove(this);
+            parent.childCount--;
+            parent.children.remove(this);
         }
     }
 
     public List<Node<T>> getChildren() {
-        return this.children;
+        return children;
     }
 
     public int hashCode() {
         int p = 239017;
-        int res = Objects.hashCode(this.value) + Objects.hashCode(getParent().value) * p;
-        for (Node<T> obj : this.getChildren()) {
+        int res = Objects.hashCode(value) + Objects.hashCode(parent.value) * p;
+        for (Node<T> obj : children) {
             res += obj.hashCode();
         }
         return res;
