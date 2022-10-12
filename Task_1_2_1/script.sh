b5c74d65c35fef13c39f052069ca719335098a7a
Nikolay <58514394+nocarend@users.noreply.github.com>
2022-10-12 16:13:11 +0300

    Add files via upload

---
+++ Task_1_2_1/script.sh
@@ -0,0 +1,7 @@
+cd src/main/java
+javac com/example/heapsort/MainHeapSort.java
+java com.example.heapsort.MainHeapSort
+javadoc -d ./../../../mainjavadoc com.example.heapsort
+rm com/example/heapsort/MainHeapSort.class
+#cd ./../../test/java
+#javadoc -d ./../../../testjavadoc com.example.heapsort
