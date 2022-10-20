81b9026a9d041b805395cdc58ae2fd6f5bd601c2
nocarend <nocarend@users.noreply.github.com>
2022-10-20 06:44:14 +0000

    Deploying to gh-pages from @ nocarend/OOP@4bd8bddfbe133a0737bdc52792ca64e2ef62f145 🚀

--- Task_1_2_3/package-search-index.js
+++ Task_1_2_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"valikov.grlib"},{"l":"valikov.grlib.representation"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"valikov.grlib.defgraph"},{"l":"valikov.grlib.intgraph"},{"l":"valikov.grlib.representation"}];updateSearchResults();
