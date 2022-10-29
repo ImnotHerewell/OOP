a15451ab49aba1d35faaa8dbb493c9004a8c4cf4
nocarend <nocarend@users.noreply.github.com>
2022-10-29 10:40:10 +0000

    Deploying to gh-pages from @ nocarend/OOP@a98338a7c58181a38ecb3909a3ae105ec97c2bdf 🚀

---
+++ Task_1_5_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.valikov","l":"Calculator"},{"p":"ru.nsu.valikov","l":"ComplexNumber"},{"p":"ru.nsu.valikov","l":"Degree"},{"p":"ru.nsu.valikov","l":"Pair"}];updateSearchResults();
