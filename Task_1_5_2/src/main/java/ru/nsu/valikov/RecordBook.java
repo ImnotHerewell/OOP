62e675f73be9cb718ac036d3e0808d30a47f974c
nocarend <58514394+nocarend@users.noreply.github.com>
2022-11-02 08:56:25 +0300

    initial

--- Task_1_5_2/src/main/java/ru/nsu/valikov/RecordBook.java
+++ Task_1_5_2/src/main/java/ru/nsu/valikov/RecordBook.java
@@ -1,4 +1,33 @@
 package ru.nsu.valikov;
 
-public class RecordBook implements Book{
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * {datetime: {title : description}}
+ */
+public class RecordBook implements Book {
+    private Map<LocalDateTime, String> records;
+
+
+    public void addRecord(String title, String description) {
+        LocalDateTime currentDate = LocalDateTime.now();
+    }
+
+    public void showRecords() {
+
+    }
+
+    public void showRecords(LocalDateTime from, LocalDateTime to, List<String> titleList) {
+        for (var title : titleList) {
+
+        }
+    }
+
+    public void deleteRecord(String title) {
+
+    }
+
+
 }
