1e1589070fcfef9798d851e5f5534299c058cf6b
nocarend <nocarend@users.noreply.github.com>
2023-02-02 04:54:26 +0000

    Deploying to gh-pages from @ nocarend/OOP@d3db6204294e3ad2bf76af7116b4dbd2513d53c7 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.valikov","l":"Primes"},{"p":"ru.nsu.valikov","l":"PrimeStatus"},{"p":"ru.nsu.valikov","l":"PrimeThread"}];updateSearchResults();
