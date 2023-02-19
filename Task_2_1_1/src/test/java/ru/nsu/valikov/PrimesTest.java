90789024517d8a8ae240984b4b7cb70d31a89cb7
nocarend <58514394+nocarend@users.noreply.github.com>
2023-02-19 20:16:20 +0300

    tests optimization
    ...

--- Task_2_1_1/src/test/java/ru/nsu/valikov/PrimesTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/valikov/PrimesTest.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -29,6 +31,7 @@ class PrimesTest {
             new Primes(HUNDRED_THOUSAND_GENERATOR.randomList());
     private static final Primes randomTestCaseMillion = new Primes(MILLION_GENERATOR.randomList());
     */
+    private static long time;
 
     private final Primes testTen = new Primes(readList("10"));
     private final Primes testHundred = new Primes(readList("100"));
@@ -49,290 +52,215 @@ class PrimesTest {
         return listOfPrimes;
     }
 
-    private void timeCalculation(final long startTime) {
-        System.out.println((System.nanoTime() - startTime));
-        //        return System.nanoTime();
+    @BeforeEach
+    public void timeCalculation() {
+        time = System.nanoTime();
+    }
+
+    @AfterEach
+    public void timePrint() {
+        System.out.println(System.nanoTime() - time);
     }
 
     @Test
     void checkTenSerial() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTen.serialCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkTenParallel() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTen.parallelCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkTenThreadsThree() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTen.threadsCheck(3));
-        timeCalculation(start);
     }
 
     @Test
     void checkTenThreadsFive() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTen.threadsCheck(5));
-        timeCalculation(start);
     }
 
     @Test
     void checkTenThreadsTen() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTen.threadsCheck(10));
-        timeCalculation(start);
     }
 
     @Test
     void checkTenThousandsSerial() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTenThousands.serialCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkTenThousandsParallel() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTenThousands.parallelCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkTenThousandsThreadsThree() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTenThousands.threadsCheck(3));
-        timeCalculation(start);
     }
 
     //255058200
     //297380300
     @Test
     void checkTenThousandsThreadsFive() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTenThousands.threadsCheck(5));
-        timeCalculation(start);
     }
 
     @Test
     void checkTenThousandsThreadsTen() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTenThousands.threadsCheck(10));
-        timeCalculation(start);
     }
 
     @Test
     void checkTenThousandsThreadsTwenty() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTenThousands.threadsCheck(20));
-        timeCalculation(start);
     }
 
     @Test
     void checkTenThousandsThreadsForty() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testTenThousands.threadsCheck(40));
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredSerial() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundred.serialCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredParallel() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundred.parallelCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThreadsThree() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundred.threadsCheck(3));
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThreadsFive() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundred.threadsCheck(5));
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThreadsTen() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundred.threadsCheck(10));
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThreadsTwenty() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundred.threadsCheck(20));
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThreadsForty() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundred.threadsCheck(40));
-        timeCalculation(start);
     }
 
     @Test
     void checkThousandSerial() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testThousand.serialCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkThousandParallel() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testThousand.parallelCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkThousandThreadsThree() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testThousand.threadsCheck(3));
-        timeCalculation(start);
     }
 
     @Test
     void checkThousandThreadsFive() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testThousand.threadsCheck(5));
-        timeCalculation(start);
     }
 
     @Test
     void checkThousandThreadsTen() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testThousand.threadsCheck(10));
-        timeCalculation(start);
     }
 
     @Test
     void checkThousandThreadsTwenty() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testThousand.threadsCheck(20));
-        timeCalculation(start);
     }
 
     @Test
     void checkThousandThreadsForty() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testThousand.threadsCheck(40));
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThousandsSerial() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundredThousands.serialCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThousandsParallel() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundredThousands.parallelCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThousandsThreadsThree() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundredThousands.threadsCheck(3));
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThousandsThreadsFive() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundredThousands.threadsCheck(5));
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThousandsThreadsTen() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundredThousands.threadsCheck(10));
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThousandsThreadsTwenty() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundredThousands.threadsCheck(20));
-        timeCalculation(start);
     }
 
     @Test
     void checkHundredThousandsThreadsForty() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testHundredThousands.threadsCheck(40));
-        timeCalculation(start);
     }
 
     @Test
     void checkFiveMillionSerial() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testFiveMillion.serialCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkFiveMillionParallel() {
-        long start = System.nanoTime();
         Assertions.assertTrue(testFiveMillion.parallelCheck());
-        timeCalculation(start);
     }
 
     @Test
     void checkFiveMillionThreadsThree() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testFiveMillion.threadsCheck(3));
-        timeCalculation(start);
     }
 
     @Test
     void checkFiveMillionThreadsFive() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testFiveMillion.threadsCheck(5));
-        timeCalculation(start);
     }
 
     @Test
     void checkFiveMillionThreadsTen() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testFiveMillion.threadsCheck(10));
-        timeCalculation(start);
     }
 
     @Test
     void checkFiveMillionThreadsTwenty() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testFiveMillion.threadsCheck(20));
-        timeCalculation(start);
     }
 
     @Test
     void checkFiveMillionThreadsForty() throws InterruptedException {
-        long start = System.nanoTime();
         Assertions.assertTrue(testFiveMillion.threadsCheck(40));
-        timeCalculation(start);
     }
 }
