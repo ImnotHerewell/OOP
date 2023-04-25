512bdece6247204affe119912ebe8f8ce2732ac8
nocarend <gudvin0203@gmail.com>
2023-04-25 18:01:30 +0300

    codestyle omg

--- Task_2_3_1/src/main/java/ru/nsu/valikov/petukhon/PetukhonMainMenu.java
+++ Task_2_3_1/src/main/java/ru/nsu/valikov/petukhon/PetukhonMainMenu.java
@@ -9,7 +9,7 @@ import java.util.List;
 import javafx.collections.FXCollections;
 
 /**
- * @author LeeWyatt 游戏的主菜单场景
+ * My main menu.
  */
 public class PetukhonMainMenu extends FXGLDefaultMenu {
 
@@ -21,33 +21,33 @@ public class PetukhonMainMenu extends FXGLDefaultMenu {
     @Override
     public void onCreate() {
         createLengthCheckBox();
-//        createSizeCheckBox();
+        //        createSizeCheckBox();
     }
 
-//    private void createSizeCheckBox() {
-//        var winBox = getUIFactoryService().newButton("RESOLUTION");
-//        winBox.setLayoutX(50);
-//        PetukhonApplication.setIntProperty("sizeX", 700);
-//        PetukhonApplication.setIntProperty("sizeY", 700);
-//        winBox.setOnAction(event -> {
-//            var node = getUIFactoryService().newChoiceBox(
-//                FXCollections.observableList(
-//                    List.of(new Pair(600, 480), new Pair<>(700, 700), new Pair(1600, 900),
-//                        new Pair(1920, 1080))));
-//            node.setOnAction(
-//                nodeEvent -> {
-//                    PetukhonApplication.setIntProperty("sizeX",
-//                        (Integer) node.getValue().getFirst());
-//                    PetukhonApplication.setIntProperty("sizeY",
-//                        (Integer) node.getValue().getSecond());
-//                });
-//            node.setLayoutX(50 * 6);
-//            node.setLayoutY(getAppHeight() / 1.4);
-//            getContentRoot().getChildren().add(node);
-//        });
-//        winBox.setLayoutY(getAppHeight() / 1.4);
-//        getContentRoot().getChildren().add(winBox);
-//    }
+    //    private void createSizeCheckBox() {
+    //        var winBox = getUIFactoryService().newButton("RESOLUTION");
+    //        winBox.setLayoutX(50);
+    //        PetukhonApplication.setIntProperty("sizeX", 700);
+    //        PetukhonApplication.setIntProperty("sizeY", 700);
+    //        winBox.setOnAction(event -> {
+    //            var node = getUIFactoryService().newChoiceBox(
+    //                FXCollections.observableList(
+    //                    List.of(new Pair(600, 480), new Pair<>(700, 700), new Pair(1600, 900),
+    //                        new Pair(1920, 1080))));
+    //            node.setOnAction(
+    //                nodeEvent -> {
+    //                    PetukhonApplication.setIntProperty("sizeX",
+    //                        (Integer) node.getValue().getFirst());
+    //                    PetukhonApplication.setIntProperty("sizeY",
+    //                        (Integer) node.getValue().getSecond());
+    //                });
+    //            node.setLayoutX(50 * 6);
+    //            node.setLayoutY(getAppHeight() / 1.4);
+    //            getContentRoot().getChildren().add(node);
+    //        });
+    //        winBox.setLayoutY(getAppHeight() / 1.4);
+    //        getContentRoot().getChildren().add(winBox);
+    //    }
 
     private void createLengthCheckBox() {
         var winBox = getUIFactoryService().newButton("LENGTH FOR WIN");
