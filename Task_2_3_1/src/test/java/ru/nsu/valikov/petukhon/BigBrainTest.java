bc8feacadb124d308dc438fb8b9af17c5ee923b4
nocarend <gudvin0203@gmail.com>
2023-05-01 17:40:31 +0300

    add jacoco support

---
+++ Task_2_3_1/src/test/java/ru/nsu/valikov/petukhon/BigBrainTest.java
@@ -0,0 +1,11 @@
+package ru.nsu.valikov.petukhon;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BigBrainTest {
+    @Test
+    public void hehe_Test() {
+        Assertions.assertEquals(Integer.MAX_VALUE, 2147483647);
+    }
+}
