781e47d99b699f2e4892d72e0326c9dd14d68ee1
nocarend <gudvin0203@gmail.com>
2023-05-01 17:41:39 +0300

    javagoc description

--- Task_2_3_1/src/test/java/ru/nsu/valikov/petukhon/BigBrainTest.java
+++ Task_2_3_1/src/test/java/ru/nsu/valikov/petukhon/BigBrainTest.java
@@ -3,6 +3,9 @@ package ru.nsu.valikov.petukhon;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Legend.
+ */
 public class BigBrainTest {
     @Test
     public void hehe_Test() {
