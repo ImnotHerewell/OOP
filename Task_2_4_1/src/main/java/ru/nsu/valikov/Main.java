5166e6f607dcc8cfb3132bba2071f403a7abc786
nocarend <gudvin0203@gmail.com>
2023-05-30 09:46:34 +0300

    add main function to execute program without tests and other shit

--- Task_2_4_1/src/main/java/ru/nsu/valikov/Main.java
+++ Task_2_4_1/src/main/java/ru/nsu/valikov/Main.java
@@ -1,29 +1,52 @@
 package ru.nsu.valikov;
 
+import java.io.File;
+import lombok.val;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import ru.nsu.valikov.models.Group;
+import ru.nsu.valikov.models.IssuedTask;
+import ru.nsu.valikov.models.Student;
+import ru.nsu.valikov.models.Task;
 import ru.nsu.valikov.service.GitService;
 import ru.nsu.valikov.service.GradleService;
-import ru.nsu.valikov.service.dto.BuildProjectRequest;
-import ru.nsu.valikov.service.dto.CheckStyleRequest;
+import ru.nsu.valikov.service.GroovyExecutor;
+import ru.nsu.valikov.service.dto.CheckTestsRequest;
 import ru.nsu.valikov.service.dto.CloneProjectRequest;
+import ru.nsu.valikov.service.dto.CompileProjectRequest;
 import ru.nsu.valikov.service.dto.GenerateDocumentationRequest;
+import ru.nsu.valikov.service.generator.ReportGeneratorService;
 
 public class Main {
 
     public static void main(String[] args) throws GitAPIException {
         try {
-            System.out.println(GitService.cloneProject(
-                new CloneProjectRequest("https://github.com/nocarend/oop_workshop",
-                    "main", "nocarend")));
-
-//            System.out.println(
-//                GradleService.buildProject(new BuildProjectRequest("nocarend/example")));
-//            System.out.println(GradleService.generateDocumentation(
-//                new GenerateDocumentationRequest("nocarend/example")));
-            System.out.println(GradleService.checkStyle(
-                new CheckStyleRequest("nocarend/example")));
+            GroovyExecutor.execute("FullConfig.groovy");
+            val students = Student.studentMap;
+            var tasks = IssuedTask.taskMap;
+            for (val task : tasks.values()) {
+                val taskStudents = task.getStudents();
+                for (val student : taskStudents) {
+                    val info = students.get(student);
+                    val isCloned = GitService.cloneProject(
+                        new CloneProjectRequest(info.getRepositoryUrl(), info.getBranchName(),
+                            info.getNickName()));
+                    val projectName =
+                        info.getNickName() + "/" + Task.taskMap.get(task.getTaskId()).getName();
+                    val isCompiled = GradleService.compileProject(new CompileProjectRequest(
+                        projectName));
+                    val isDocumented = GradleService.generateDocumentation(
+                        new GenerateDocumentationRequest(projectName));
+                    val testValue = GradleService.getCoefficientOfPassedTests(
+                        new CheckTestsRequest(projectName));
+//                    val checkStyleValue = GradleService.getCoefficientOfCheckStyle(
+//                        new CheckTestsRequest(projectName));
+                    Utils.deleteDirectory(
+                        new File(Configuration.DEFAULT_TASKS_DIRECTORY + "/" + info.getNickName()));
+                }
+            }
+            Group.groups.keySet().forEach(ReportGeneratorService::generateAttendance);
         } finally {
-//            Utils.deleteDirectory(Configuration.DEFAULT_TASKS_DIRECTORY);
+            Utils.deleteDirectory(Configuration.DEFAULT_TASKS_DIRECTORY);
         }
     }
 }
