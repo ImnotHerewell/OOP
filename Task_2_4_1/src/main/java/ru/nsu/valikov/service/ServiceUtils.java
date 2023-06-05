613ce0ab8986db8f9ef22766b3abace45278a7ac
nocarend <gudvin0203@gmail.com>
2023-06-05 21:14:56 +0300

    delete useless use

--- Task_2_4_1/src/main/java/ru/nsu/valikov/service/ServiceUtils.java
+++ Task_2_4_1/src/main/java/ru/nsu/valikov/service/ServiceUtils.java
@@ -6,10 +6,5 @@ import lombok.experimental.UtilityClass;
 public class ServiceUtils {
 
     static final String SLASH = "/";
-    static final String COMMAND_CD = "cd";
-    static final String AMPERSAND = "&";
-    static final String GRADLE_BAT = "gradlew.bat";
-    static final String SLASH_C = "/c";
     static final String BUILD = "build";
-    static final String TMP = "tmp";
 }
